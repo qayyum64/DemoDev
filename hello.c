94bde30173f3980856aed9d77fa74269882cc3ba
Abdul Qayyum <qayyum.abdul@peopletech.com>
2025-10-28 18:26:03 +0530

    3rd commit

--- hello.c
+++ hello.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 int main(){
     printf("Hello People Tech");
-        printf("Hello People Tech");
-        printf("Hello People Tech");
-        printf("Hello People Tech");
+        // printf("Hello People Tech");
+        // printf("Hello People Tech");
+        // printf("Hello People Tech");
+            printf("Vishnugit ");
 }
